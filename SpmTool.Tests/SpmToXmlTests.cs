3a59cc57569dc0b20714489e6d10a9bd23201cbf
Jamie Cansdale <jcansdale+github@gmail.com>
2016-06-06 15:42:23 +0100

    Add a failing test...

--- SpmTool.Tests/SpmToXmlTests.cs
+++ SpmTool.Tests/SpmToXmlTests.cs
@@ -12,6 +12,12 @@ namespace SpmTool.Tests
     public class SpmToXmlTests
     {
         [Test]
+        public void FailMe()
+        {
+            Assert.Fail("Boom!!");
+        }
+
+        [Test]
         public void Convert_String()
         {
             var text =
